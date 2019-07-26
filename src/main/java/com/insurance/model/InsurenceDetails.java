c93c0ab6b7433ec70e67e1e9dbb27b2eeb26771f
marshadm <arshadmohd666@gmail.com>
2019-07-26 13:27:22 +0530

    project setup

---
+++ src/main/java/com/insurance/model/InsurenceDetails.java
@@ -0,0 +1,19 @@
+package com.insurance.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="INSURANCE_DETAILS")
+@Data
+public class InsurenceDetails {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long insuranceId;
+	
+}
